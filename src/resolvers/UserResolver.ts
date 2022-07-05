340bbe813624169959fd9745739d87ff77bc60ec
BAM-hub <bsharamin12@gmail.com>
2022-07-05 13:37:51 +0300

    working on create user

---
+++ src/resolvers/UserResolver.ts
@@ -0,0 +1,37 @@
+import { PrismaClient } from "@prisma/client";
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import { User } from "../schema/UserSchema";
+import { Gender as PrismaGender } from "prisma/prisma-client";
+import { DateScalar } from "../Scalars/dateScalar";
+
+const prisma = new PrismaClient();
+
+@Resolver(User)
+export class UserResolver {
+  // find all users
+
+  @Query(() => User)
+  async users(): Promise<User | null> {
+    return null;
+  }
+
+  @Mutation(() => User)
+  async createUser(
+    @Arg("name") name: string,
+    @Arg("email") email: string,
+    @Arg("gender") gender: PrismaGender,
+    @Arg("country") country: string,
+    @Arg("birthday", () => DateScalar) birthday: Date
+  ): Promise<User | null> {
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        gender,
+        country,
+        birthday,
+      },
+    });
+    return user;
+  }
+}
