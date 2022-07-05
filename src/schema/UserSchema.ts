633fdeec8770f17077decfd7e6b5d24aa76dc1bb
BAM-hub <bsharamin12@gmail.com>
2022-07-05 13:38:15 +0300

    ts & gql user schema

---
+++ src/schema/UserSchema.ts
@@ -0,0 +1,33 @@
+import { Field, ObjectType, registerEnumType } from "type-graphql";
+import { Gender } from "prisma/prisma-client";
+import { DateScalar } from "../Scalars/dateScalar";
+
+registerEnumType(Gender, {
+  name: "Gender",
+});
+
+@ObjectType()
+export class User {
+  @Field(() => Number)
+  id: Number;
+
+  @Field(() => String)
+  name: String;
+
+  @Field(() => Gender)
+  gender: Gender;
+
+  @Field(() => String)
+  email: String;
+
+  @Field(() => String, { nullable: true })
+  country: string | null;
+
+  @Field(() => DateScalar)
+  birthday: Date;
+
+  @Field(() => Date, { defaultValue: Date.now(), nullable: true })
+  date?: Date;
+
+  // Todo Posts
+}
