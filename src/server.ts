2fe399ad2f807c23d559e8f45a1607df9436720b
BAM-hub <bsharamin12@gmail.com>
2022-07-05 13:36:24 +0300

    init graphql server

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,29 @@
 import { PrismaClient } from "@prisma/client";
+import "reflect-metadata";
+import * as express from "express";
+import { graphqlHTTP } from "express-graphql";
+import { buildSchema } from "type-graphql";
+import { UserResolver } from "./resolvers/UserResolver";
 
 const prisma = new PrismaClient();
+const app = express();
 
-async function main() {
-  const user = await prisma.user.findMany();
-  console.log(user);
-  // ... you will write your Prisma Client queries here
-}
+const main = async () => {
+  const schema = await buildSchema({
+    resolvers: [UserResolver],
+  });
+  app.use(
+    "/graphql",
+    graphqlHTTP({
+      schema,
+      graphiql: true,
+    })
+  );
+
+  const PORT = 8000;
+
+  app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+};
 
 main()
   .catch((e) => {
