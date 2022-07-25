7378371676aaf4bdb2711007ff87e3354e806671
BAM-hub <bsharamin12@gmail.com>
2022-07-25 23:54:00 +0300

    switched to using of post inpyu type

--- src/resolvers/PostResolver.ts
+++ src/resolvers/PostResolver.ts
@@ -1,7 +1,6 @@
 import { Arg, Mutation, Query, Resolver } from "type-graphql";
-import { PostType, PrismaClient } from "@prisma/client";
-import { Post, Pool } from "../schema/PostSchema";
-import { DateScalar } from "../Scalars/dateScalar";
+import { PrismaClient } from "@prisma/client";
+import { Post, PostInput } from "../schema/PostSchema";
 
 const prisma = new PrismaClient();
 
@@ -21,31 +20,11 @@ export class PostResolver {
 
   // create post
   @Mutation(() => Post)
-  async createPost(
-    @Arg("userId") userId: number,
-    @Arg("comunityId", () => Number, { nullable: true }) comunityId: number,
-    @Arg("title") title: string,
-    @Arg("text", () => String, { nullable: true }) text: string,
-    @Arg("publishedAt", () => DateScalar, { nullable: true }) publishedAt: Date,
-    @Arg("image", () => String, { nullable: true }) image: string,
-    @Arg("link", () => String, { nullable: true }) link: string,
-    @Arg("notfications") notfications: boolean,
-    @Arg("isPublished", () => Boolean) isPublished: boolean,
-    @Arg("type", () => PostType) type: PostType
-  ): Promise<Post | null> {
+  async createPost(input: PostInput): Promise<Post | null> {
     try {
       const post = await prisma.post.create({
         data: {
-          userId,
-          comunityId: 0,
-          type,
-          title,
-          text,
-          publishedAt,
-          image,
-          link,
-          notification: notfications,
-          published: isPublished,
+          ...input,
         },
       });
       return post;
